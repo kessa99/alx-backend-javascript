904d62da3802dd00dc763c3ff9b241093af1a8f6
root <kessadavidkipre@gmail.com>
2024-01-02 19:47:27 -0800

    task

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+function getListStudents() {
+  return [
+    { id: 1, firstname: "Guillaume", location: "San Francisco" },
+    { id: 2, firstname: "James", location: "Columbia" },
+    { id: 3, firstaname: "Serena", location: "San Francisco" },
+  ];
+}
+
+export default getListStudents;
