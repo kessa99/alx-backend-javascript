4464e753bd89a44838509c8676cfca253c6b903c
root <kessadavidkipre@gmail.com>
2024-01-04 01:18:44 -0800

    2-arrow.js

---
+++ 0x00-ES6_basic/2-main.js
@@ -0,0 +1,5 @@
+import GetNeighborhoodsList from './2-arrow.js';
+
+const neighborhoodsList = new GetNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
