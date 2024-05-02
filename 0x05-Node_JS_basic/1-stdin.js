b6c107815b68de911f4fff34a1dff2306a3fcb72
kessa99 <kessadavidkipre@gmail.com>
2024-05-02 17:24:16 +0000

    Handle EOF

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,13 @@
 process.stdout.write('Welcome to Hoblberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  if (name) {
+  if (name !== null) {
     process.stdout.write(`Your name is: ${name}`);
+  } else {
+    process.stdin.emit('end');
   }
 });
 
-process.stdin.on('close', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
