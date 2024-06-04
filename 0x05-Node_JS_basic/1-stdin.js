6bd9cd5b9a7d7ddbcb9b4cdb41605d690521d1d6
kessa99 <kipre325@gmail.com>
2024-06-04 15:25:49 +0000

    update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,6 @@
 process.stdout.write('Welcome to Hoblberton School, what is your name?\n');
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  if (name !== null) {
-    process.stdout.write(`Your name is: ${name}`);
-  } else {
-    process.stdin.emit('end');
-  }
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
