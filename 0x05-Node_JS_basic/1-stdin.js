c7f28a8ceb043417529e4d181575358e1556ffa6
kessa99 <kipre325@gmail.com>
2024-06-04 16:43:56 +0000

    update stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,12 @@
 const process = require('node:process');
-process.stdout.write('Welcome to Hoblberton School, what is your name?\n');
-process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data}`);
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`)
+  }
 });
 
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+process.stdin.on('close', () => {
+  process.stdout.write('this important softawre is now closing\n');
 });
