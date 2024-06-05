bc960c9de3b7339c90810c971799d6a7a2c6552f
kessa99 <kipre325@gmail.com>
2024-06-05 09:46:24 +0000

    update 5-https

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,30 +1,36 @@
 const http = require('http');
+
 const countStudents = require('./3-read_file_async');
+
 const port = 1245;
 const hostname = 'localhost';
+
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    switch (req.url) {
-        case '/':
-            res.write('Hello Holberton School!');
-            res.end();
-            break;
-        case '/students':
-            res.write('This is the list of our students\n');
-            res.end();
-            try {
-                countStudents(process.argv[2]);
-            } catch (error) {
-                res.end(error.message);
-            }
-            break;
-        default:
-            break;
-    }
-    app.listen(port, hostname, () => {
-        console.log(`Server running at http://${hostname}:${port}/`);
-    });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2])
+      .then((data) => {
+        res.write(data);
+        res.end();
+      })
+      .catch((error) => {
+        res.statusCode = 500;
+        res.write(error.message);
+        res.end();
+      });
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
