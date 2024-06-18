c9ef34e5311d9f7f7963307e369e3e6a4d8a8477
kessa99 <kipre325@gmail.com>
2024-06-18 16:15:27 +0000

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
-const process = require('node:process');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  if (name !== null) {
-    process.stdout.write(`Your name is: ${name}`)
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`
   }
 });
 
-process.stdin.on('close', () => {
+process.stdin.on('end', () => {
   process.stdout.write('this important softawre is now closing\n');
 }); 
