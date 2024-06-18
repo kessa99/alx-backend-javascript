ad2596be50bf475f3ef83bbd21e41ec32682df09
kessa99 <kipre325@gmail.com>
2024-06-18 16:18:01 +0000

    update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name) {
-    process.stdout.write(`Your name is: ${name}`
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
